019ff1e30817458336432e757821a0d3a7c13900
simon_r <symokevo@gmail.com>
2025-08-17 11:06:23 +0300

    Create the products model and wire the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
+  resources :products
+  get "/products", to: "products#index"
+  get "/products/:id", to : "products#show"
+  get "/products/new", to: "products#new"
+  get "/products/:id/edit", to: "products#edit"
+  post "/products", to: "products#create"
+  patch "/products/:id", to: "products#update"
+  put "/products/:id", to: "products#update"
+  delete "/products/:id", to: "products#destroy"
   # root "posts#index"
 end
