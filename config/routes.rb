ef93f5a262250b30f4b345b594b1711cef357fcf
simon_r <symokevo@gmail.com>
2025-08-17 12:42:17 +0300

    Implement the products controller actions and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   resources :products
   get "/products", to: "products#index"
-  get "/products/:id", to : "products#show"
+  get "/products/:id", to: "products#show"
   get "/products/new", to: "products#new"
   get "/products/:id/edit", to: "products#edit"
   post "/products", to: "products#create"
   patch "/products/:id", to: "products#update"
   put "/products/:id", to: "products#update"
   delete "/products/:id", to: "products#destroy"
-  # root "posts#index"
+  root "products#index"
 end
